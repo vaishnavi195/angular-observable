9c082c9e106ca8cdec2261ad7332dfff41c62c0d
vaishnavi195 <varshitha.345@gmail.com>
2022-05-16 16:12:16 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component, VERSION } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  constructor() {
+    const test$ = new Observable((sub) => {
+      sub.next('vaish'), sub.next('var');
+    });
+
+    test$.subscribe((x) => {
+      console.log('1', x);
+    });
+    test$.subscribe((y) => {
+      console.log('2', y);
+    });
+  }
 }
